1bfe9d977385381cea38445b175d107321f3d4f6
soukainanas <51346846+soukainanas@users.noreply.github.com>
2023-01-20 10:33:15 +0100

    Add files via upload

---
+++ agilite_VF/agilite_final/test_unitaire.py
@@ -0,0 +1,9 @@
+import unittest
+from agilite_mairie.test_unitaire_mairie import MairieTestCase
+from agilite_etudiant.test_unitaire_etudiant import Test_etudiant, Test_cahier
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
